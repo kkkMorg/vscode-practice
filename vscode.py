83e17c4044951b5ba6cc2147aa7eb0d554a6479c
kkkMorg <Morg.k.0104@gmail.com>
2025-11-14 15:01:18 +0900

    fix: explain sequence in English

--- vscode.py
+++ vscode.py
@@ -2,4 +2,4 @@ for i in range(1, 15 + 1):
     if i % 3 == 0 or i % 5 == 0:
         print('fizz' * (i % 3 == 0) + 'buzz' * (i % 5 == 0))
     else:
-        print(f'{i}번 째 입니다.')
+        print(f'{i}th number')
